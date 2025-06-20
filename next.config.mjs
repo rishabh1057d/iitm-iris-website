4ff4a51ca21f8b3019ea6a2f62c7270049a3e3db
rishabh1057d <rishabhsingh1057d@gmail.com>
2025-06-21 00:03:37 +0530

    changes made over

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['placeholder.svg', 'blob.v0.dev'],
     dangerouslyAllowSVG: true,
@@ -14,7 +11,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
